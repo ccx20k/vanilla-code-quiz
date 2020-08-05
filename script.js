a59873ec6bcbe1123c9e023327b8262b15c9d2bf
boido.boido <coreycapstick72@gmail.com>
2020-08-04 23:42:26 -0400

    added the button event listener it now: hides the main card when clicked. shows the quiz header with timer and score board when clicked. shows the question box when clicked. I hae lso added a timer and a display scores fuction which displays when the timer reachers zero.

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
 //declare global variables
-var questions =  [
+var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
     },
     ///etc.
-  ];
+];
 var timerEl = document.querySelector("#timer");
 var secondsLeft = 10;
+var scoreCard = document.querySelector("#endOfQuiz");
+var questionBox = document.querySelector("#questionBox");
+var daButton = document.querySelector("#startQuiz");
+var quizHead = document.querySelector("#quiz-header");
+var theWhopper = document.querySelector("#jumbotron");
 
-  //make the timer 
-  function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
-  
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        sendMessage();
-      }
-  
+//button event listener
+daButton.addEventListener("click", function () {
+    beginTimer(); //begins timer when clicked
+    quizHead.classList.remove("d-none");
+    questionBox.classList.remove("d-none");
+    theWhopper.classList.add("d-none");
+});
+
+//make the timer 
+function beginTimer() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            dispScores();
+        }
+
     }, 1000);
-  }
+};
+
+//display score board
+function dispScores() {
+    scoreCard.classList.remove("d-none");
+
+};
