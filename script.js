e78e5faf7cac947de78ac5caf065b44b98127a14
boido.boido <coreycapstick72@gmail.com>
2020-08-04 23:15:49 -0400

    I've remade this this so many times im not doing an initial commit. i copied some other students html and remade it for my own needs. I am now copy and pasting solved activities from the content.

---
+++ script.js
@@ -0,0 +1,30 @@
+//declare global variables
+var questions =  [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    ///etc.
+  ];
+var timerEl = document.querySelector("#timer");
+var secondsLeft = 10;
+
+  //make the timer 
+  function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
